f22d10ea34a55219c3852a7a5f41d7271312e57c
tarun-stk <tarunstk8897@gmail.com>
2024-07-25 00:07:13 +0530

    added new classes

---
+++ 05-multiple-inheritance/multiple-inheritance/src/com/inheritance/B.java
@@ -0,0 +1,8 @@
+package com.inheritance;
+
+public class B implements A{
+    @Override
+    public void print() {
+        System.out.println("Printing!!");
+    }
+}
---
+++ 07-transaction-management-spring/example/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.example to javafx.fxml;
+    exports com.example.example;
+}
